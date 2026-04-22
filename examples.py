159b60cec0da00a09de0f3e4f8f7191c7907ccea
Kevin Carrogan <kevin@carrogan.com>
2026-04-22 08:57:13 +0100

    Rename branch fallback keyword from otherwise to default

--- examples.py
+++ examples.py
@@ -2,13 +2,10 @@ class Wizard:
     def __init__(self, **configuration):
         pass
 
-    def start(self, form):
+    def step(self, form):
         return self
     
-    def then(self, form):
-        return self
-    
-    def branch(self, *forms, otherwise=None):
+    def branch(self, *forms, default=None):
         return self
     
 
@@ -94,28 +91,28 @@ def condition(cond, flow):
 
 that_wizard = (
     Wizard()
-    .start(BWizardFirstForm)
+    .step(BWizardFirstForm)
     .branch(
         condition(is_this, BWizardSecondForm),
-        otherwise=BWizardThirdForm,
+        default=BWizardThirdForm,
     )
 )
 
 
 main_wizard = (
     Wizard()  # This will be used for high-level configuration
-    .start(FirstForm)  # This could possibly be a view instead (need to work out that concept) - also starts to feel annoying to have to `start` each time, but maybe that's explicit
-    .then(SecondForm)
-    .then(ThirdForm)
+    .step(FirstForm)  # This could possibly be a view instead (need to work out that concept)
+    .step(SecondForm)
+    .step(ThirdForm)
     .branch(
         condition(
             is_this,
-            Wizard().start(AWizardFirstForm).then(AWizardSecondForm),
+            Wizard().step(AWizardFirstForm).step(AWizardSecondForm),
         ),
         condition(is_that, that_wizard),
         # If neither condition is met then this would be skipped instead of erroring
     )
-    .then(MyFinalForm)
+    .step(MyFinalForm)
 )
 
 
@@ -136,8 +133,8 @@ class ManagementFormClass:
 
 configured = (
     Wizard(management_form_class=ManagementFormClass)
-    .start(FirstForm)
-    .then(SecondForm)
+    .step(FirstForm)
+    .step(SecondForm)
 )
 
 
@@ -168,6 +165,6 @@ class FirstFormView:
 
 view_based = (
     Wizard()
-    .start(FirstFormView)
-    .then(SecondForm)  # Under the hood this is just automatically generating the FormView for us, but each step _is_ a FormView (or something that matches that contract)
+    .step(FirstFormView)
+    .step(SecondForm)  # Under the hood this is just automatically generating the FormView for us, but each step _is_ a FormView (or something that matches that contract)
 )
