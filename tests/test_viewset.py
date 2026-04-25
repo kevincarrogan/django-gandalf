b4b4b9ddbd84af0be6adb839f62ab7f857702e40
Kevin Carrogan <kevin@carrogan.com>
2026-04-25 22:17:33 +0100

    Assert wizard form rendering

--- tests/test_viewset.py
+++ tests/test_viewset.py
@@ -1,20 +1,15 @@
-from pytest_django.asserts import assertTemplateUsed
+from pytest_django.asserts import assertContains, assertTemplateUsed
 
 from tests.testapp.forms import FirstStepForm
 
 
-def test_wizard_viewset_renders_template(client):
+def test_wizard_viewset_renders_form(client):
     response = client.get("/wizard/")
 
     assert response.status_code == 200
     assertTemplateUsed(response, "testapp/single_step_wizard.html")
-
-
-def test_wizard_viewset_renders_first_step_form(client):
-    response = client.get("/wizard/")
-
-    assert response.status_code == 200
     assert isinstance(response.context["form"], FirstStepForm)
+    assertContains(response, '<input type="text" name="name"')
 
 
 def test_wizard_viewset_delegates_post_to_first_step_form(client):
